8877f59fc1e59485b8c0a7785928d92c7be58cc8
Alejandro <alejandro.ocampo2@udea.edu.co>
2024-11-24 00:23:58 -0500

    HandTracking mediapipe

--- HandTracking.py
+++ HandTracking.py
@@ -1,18 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 23 22:03:21 2024
-
-@author: ocamp
-"""
 import cv2
 import mediapipe as mp
 import time
 
-
+# Inicializar la captura de video
 cap = cv2.VideoCapture(0)
+
+# Inicializar MediaPipe Hands
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while True:
     success, img = cap.read()
+    if not success:
+        break
     
-    cv2.imshow("image", img)
-    cv2.waitkey(1)
+    # Convertir la imagen a RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    # Procesar la imagen con MediaPipe Hands
+    results = hands.process(imgRGB)
+    
+    # Verificar si se detectaron manos
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Dibujar las marcas de las manos en la imagen
+            mp_drawing.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            #print(f'punto: {hand_landmarks}', hand_landmarks)
+    
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    
+    # Mostrar la imagen con las marcas
+    cv2.imshow("Hand Tracking", img)
+    
+    # Salir con la tecla 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Liberar recursos
+cap.release()
+cv2.destroyAllWindows()
